f2fa2862f6aaffe2874c29f0211d679e7a760fe7
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-02-08 20:18:16 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,40 @@
 input.onButtonPressed(Button.A, function () {
-    ESP8266_IoT.uploadKidsiot(77)
-    basic.pause(2000)
+    if (true) {
+        ESP8266_IoT.connectKidsiot("9e8aB6lZZR7MnUtI ", "1")
+        ESP8266_IoT.uploadKidsiot(77)
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (true) {
+        ESP8266_IoT.connectKidsiot("9e8aB6lZZR7MnUtI ", "2")
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        ESP8266_IoT.uploadKidsiot(23)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    ESP8266_IoT.uploadKidsiot(23)
-    basic.pause(2000)
+    if (true) {
+        ESP8266_IoT.connectKidsiot("9e8aB6lZZR7MnUtI ", "1")
+        ESP8266_IoT.uploadKidsiot(23)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (true) {
+        ESP8266_IoT.connectKidsiot("9e8aB6lZZR7MnUtI ", "2")
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        ESP8266_IoT.uploadKidsiot(73)
+    }
 })
 ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.KidsIotSwitchState.off, function () {
     basic.showIcon(IconNames.SmallHeart)
@@ -13,12 +43,11 @@ ESP8266_IoT.iotSwitchEvent(ESP8266_IoT.KidsIotSwitchState.on, function () {
     basic.showIcon(IconNames.Heart)
     serial.writeLine("do1")
 })
-basic.showIcon(IconNames.Square)
+basic.showIcon(IconNames.SmallDiamond)
 ESP8266_IoT.initWIFI(SerialPin.P1, SerialPin.P2, BaudRate.BaudRate115200)
 ESP8266_IoT.connectWifi("Hotspot", "password")
 basic.pause(5000)
-basic.showIcon(IconNames.SmallSquare)
-ESP8266_IoT.connectKidsiot("9e8aB6lZZR7MnUtI", "1")
+basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
     if (ESP8266_IoT.wifiState(false)) {
         basic.showIcon(IconNames.No)
